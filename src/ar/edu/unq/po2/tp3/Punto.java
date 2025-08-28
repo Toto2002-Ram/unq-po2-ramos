0a2b692a00cb67c0facdc81b5bc6ff2e95ead4e7
TotoRamos <tomasagustinramos@gmail.com>
2025-08-28 14:43:03 -0300

    Version inicial

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -2,4 +2,27 @@ package ar.edu.unq.po2.tp3;
 
 public class Punto {
 
+	 private float X;
+	 private float Y;
+	 public Punto(float x, float y) {
+		super();
+		X = x;
+		Y = y;
+	 }
+	 public float getX() {
+		 return X;
+	 }
+	 public void setX(float x) {
+		 X = x;
+	 }
+	 public float getY() {
+		 return Y;
+	 }
+	 public void setY(float y) {
+		 Y = y;
+	 }
+	 
+	 
+	 
+	 
 }
